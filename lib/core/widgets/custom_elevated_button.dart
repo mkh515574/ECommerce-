5f5f4495feb28eaccd93b2340b539a808705d6c9
mkha2 <mkh515574@gmail.com>
2025-09-13 14:51:45 +0300

    feat(auth): add login UI

---
+++ lib/core/widgets/custom_elevated_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../utils/app_colors.dart';
+
+class CustomElevatedButton extends StatelessWidget {
+  const CustomElevatedButton({
+    super.key,
+    required this.onPressed,
+    this.backgroundColor,
+    this.borderSideColor,
+    this.textStyle,
+    required this.text,
+    this.buttonContent,
+  });
+
+  final VoidCallback? onPressed;
+  final Color? backgroundColor;
+  final Color? borderSideColor;
+  final TextStyle? textStyle;
+  final String text;
+  final Widget? buttonContent;
+
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(vertical: height * 0.02),
+        backgroundColor: backgroundColor ?? AppColors.yellowColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+          side: BorderSide(color: borderSideColor ?? AppColors.yellowColor, width: 1.5),
+        ),
+      ),
+      child: Text(text),
+    );
+  }
+}
---
+++ lib/features/auth/login/login_screen.dart
@@ -0,0 +1,155 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../core/utils/app_assets.dart';
+import '../../../core/utils/app_colors.dart';
+import '../../../core/utils/app_routes.dart';
+import '../../../core/utils/app_styles.dart';
+import '../../../core/utils/validators.dart';
+import '../../../core/widgets/custom_elevated_button.dart';
+import '../../../core/widgets/custom_text_form_field.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController userNameController = TextEditingController(
+    text: "adasdfvv@fds.com",
+  );
+  TextEditingController passwordController = TextEditingController(
+    text: "18061717@A",
+  );
+  var formKey = GlobalKey<FormState>();
+
+  void login() {
+    if (formKey.currentState!.validate()) {
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 91.h,
+                  bottom: 87.h,
+                  left: 97.w,
+                  right: 97.w,
+                ),
+                child: Image.asset(AppAssets.appBarLeading),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.w),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+
+                    AutoSizeText(
+                      'Welcome Back To Route',
+                      style: AppStyles.semi24White,
+                      maxLines: 1,
+                    ),
+                    AutoSizeText(
+                      'Please sign in with your mail',
+                      style: AppStyles.light16White,
+                      maxLines: 1,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 40.h),
+                      child: Form(
+                        key: formKey,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Text("User Name", style: AppStyles.medium18White),
+                            CustomTextFormField(
+                              //isPassword: false,
+
+                              keyboardType: TextInputType.text,
+                              obscureText: false,
+                              hintText: "enter your name",
+                              hintStyle: AppStyles.light18HintText,
+                              fillColor: AppColors.whiteColor,
+                              controller: userNameController,
+                              validator: AppValidators.validateEmail,
+                            ),
+                            Text("Password", style: AppStyles.medium18White),
+                            CustomTextFormField(
+                             // isPassword: true,
+                              keyboardType: TextInputType.visiblePassword,
+                              obscureText: true,
+                              hintText: "enter your password",
+                              hintStyle: AppStyles.light18HintText,
+                              fillColor: AppColors.whiteColor,
+                              controller: passwordController,
+                              validator: AppValidators.validatePassword,
+                              suffixIcon: IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.visibility_off),
+                              ),
+                            ),
+                            InkWell(
+                              onTap: () {},
+                              child: Text(
+                                'Forgot Password',
+                                style: AppStyles.regular18White,
+                                textAlign: TextAlign.end,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(top: 35.h),
+                              child: CustomElevatedButton(
+                                backgroundColor: AppColors.whiteColor,
+                                textStyle: AppStyles.semi20Primary,
+                                text: "Login",
+                                onPressed: () {
+                                  login();
+                                },
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(top: 30.h),
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.pushReplacementNamed(
+                                    context,
+                                    AppRoutes.registerRoute,
+                                  );
+                                },
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      child: Text(
+                                        'Don’t have an account? Create Account',
+                                        style: AppStyles.medium18White,
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
