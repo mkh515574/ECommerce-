4eddb6ee7f30f5d43facd1905a0db7e605c61b35
mkha2 <mkh515574@gmail.com>
2025-09-13 15:10:13 +0300

    feat(auth): login ui done

--- lib/core/utils/app_routes.dart
+++ lib/core/utils/app_routes.dart
@@ -1,7 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../../features/auth/login/login_screen.dart';
+
 class AppRoutes {
   static String homeRoute = "home";
   static String productRoute = "productDetails";
   static String cartRoute = "cart";
   static String loginRoute = "login";
   static String registerRoute = "register";
+
+
+  static Map<String, Widget Function(BuildContext)> routes = {
+
+   // homeRoute: (context) => const HomeScreen(),
+   // productRoute: (context) => const ProductDetailsScreen(),
+    //cartRoute: (context) => const CartScreen(),
+    loginRoute: (context) => const LoginScreen(),
+    //registerRoute: (context) => const RegisterScreen(),
+
+  };
 }
